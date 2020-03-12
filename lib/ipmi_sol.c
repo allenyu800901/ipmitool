26fd186ac2f0374985f76fde591ee613fe4dd0ef
Allen.Yu <allen.ch.yu@mail.foxconn.com>
2020-03-12 09:52:54 +0800

    Debug SOL ISSUE v2
    Signed-off-by: Allen.Yu <allen.ch.yu@mail.foxconn.com>

--- lib/ipmi_sol.c
+++ lib/ipmi_sol.c
@@ -1298,6 +1298,8 @@ ipmi_sol_deactivate(struct ipmi_intf * intf, int instance)
 		return -1;
 	}
 
+	printf("################# ipmitool : Enter ipmi_sol_deactivate, line:%d \n",__LINE__);
+	
 	memset(&req, 0, sizeof(req));
 	req.msg.netfn    = IPMI_NETFN_APP;
 	req.msg.cmd      = IPMI_DEACTIVATE_PAYLOAD;
@@ -1315,6 +1317,8 @@ ipmi_sol_deactivate(struct ipmi_intf * intf, int instance)
 	data[5] = 0;
 
 	rsp = intf->sendrecv(intf, &req);
+
+	printf("################# ipmitool : Send netfn 0x49 to BMC, line:%d \n",__LINE__);
 
 	if (NULL != rsp) {
 		switch (rsp->ccode) {
@@ -1334,6 +1338,8 @@ ipmi_sol_deactivate(struct ipmi_intf * intf, int instance)
 	} else {
 		lprintf(LOG_ERR, "Error: No response de-activating SOL payload");
 	}
+
+	printf("################# ipmitool : Exit ipmi_sol_deactivate, line:%d \n",__LINE__);
 
 	return -1;
 }
